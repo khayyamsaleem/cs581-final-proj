5757195df859d18a3b1a6b58a9f7e7eba87edef1
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-12-18 17:46:36 -0500

    Added member dict

--- main.py
+++ main.py
@@ -5,17 +5,22 @@ from pprint import pprint
 import itertools
 
 load_dotenv(find_dotenv(),override=True)
-
 API_TOKEN = os.getenv("GROUPME_ACCESS_TOKEN")
 
 client = Client.from_token(API_TOKEN)
 
+def get_group_matrix():
+    groups = list(client.groups.list_all())
+    return list(itertools.product(groups, groups))
+
+def count_members():
+    return {(x.name,y.name):set(map(lambda z: z.user_id, x.members)).intersection(map(lambda z: z.user_id, y.members)) for (x,y) in get_group_matrix()}
 
 def test():
     groups = {}
     for g in client.groups.list_all():
         groups[g.name]=list(map(lambda m: m.user_id, g.members))
-    pprint(groups)
+    pprint(groups.keys())
 
 if __name__ == '__main__':
-    test()
+    pprint(count_members())
