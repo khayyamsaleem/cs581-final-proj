9cda3568799b5009abe1781ee4f26559a07ea1a5
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-12-18 18:04:41 -0500

    added df

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv, find_dotenv
 from groupy.client import Client
 from pprint import pprint
 import itertools
+import pandas as pd
+import numpy as np
 
 load_dotenv(find_dotenv(),override=True)
 API_TOKEN = os.getenv("GROUPME_ACCESS_TOKEN")
@@ -13,8 +15,18 @@ def get_group_matrix():
     groups = list(client.groups.list_all())
     return list(itertools.product(groups, groups))
 
-def count_members():
+def member_intersections():
     return {(x.name,y.name):set(map(lambda z: z.user_id, x.members)).intersection(map(lambda z: z.user_id, y.members)) for (x,y) in get_group_matrix()}
+
+def get_df_of_counts():
+    groups = list(client.groups.list_all())
+    groupnames = [x.name for x in groups]
+    df = pd.DataFrame(np.zeros((len(groups), len(groups))), index=groupnames, columns=groupnames)
+    m_i = member_intersections()
+    for (x,y) in m_i:
+        df.loc[x,y] = len(m_i[(x,y)])
+    return df
+
 
 def test():
     groups = {}
@@ -23,4 +35,4 @@ def test():
     pprint(groups.keys())
 
 if __name__ == '__main__':
-    pprint(count_members())
+    print(get_df_of_counts())
