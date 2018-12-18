68db49e5f26cd934f5046c6236eaeb97702fc198
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-12-18 15:33:12 -0500

    Added test code to work with client

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from dotenv import load_dotenv, find_dotenv
 import os
+from dotenv import load_dotenv, find_dotenv
+from groupy.client import Client
 
 load_dotenv(find_dotenv(),override=True)
 
 API_TOKEN = os.getenv("GROUPME_ACCESS_TOKEN")
 
+client = Client.from_token(API_TOKEN)
+
+def test():
+    for g in client.groups.list():
+        print(g.name)
+
+if __name__ == '__main__':
+    test()
