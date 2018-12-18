5e08c99fe9ba13fa3d57d9667cc0358280ab4637
Khayyam Saleem <khayyam.saleem@gmail.com>
2018-12-18 15:45:56 -0500

    Added pprint dep, added maps from groups to members

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 from groupy.client import Client
+from pprint import pprint
 
 load_dotenv(find_dotenv(),override=True)
 
@@ -8,9 +9,12 @@ API_TOKEN = os.getenv("GROUPME_ACCESS_TOKEN")
 
 client = Client.from_token(API_TOKEN)
 
+
 def test():
-    for g in client.groups.list():
-        print(g.name)
+    groups = {}
+    for g in client.groups.list_all():
+        groups[g.name]=list(map(lambda m: m.user_id, g.members))
+    pprint(groups)
 
 if __name__ == '__main__':
     test()
